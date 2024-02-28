096e20fc432b3d07effbc925c6596a88afb952e7
abeerkm <68709596+abeerkm@users.noreply.github.com>
2024-02-28 19:31:32 +0200

    add username an password to gitgnore

--- src/tests/auth.setup.ts
+++ src/tests/auth.setup.ts
@@ -2,13 +2,14 @@
 import { expect, test as setup } from '@playwright/test';
 import { Login } from '../logic/login';
 import config from '../config.json';
+import user from '../user.json'
 const authFile = 'playwright/.auth/user.json';
 
 setup('authenticate @login', async ({ page,context }) => {
     await context.tracing.start({ screenshots: true, snapshots: true });
     await page.goto(config.loginUrl);
     const login=new Login(page);
-    await login.fullLoginProcess(config.username,config.password);
+    await login.fullLoginProcess(user.username,user.password);
     await page.waitForURL('https://app.todoist.com/app/today');
     await page.context().storageState({ path: authFile });
     page.close();
